755b3120bde6a34aef8ffa4f31234debf13ee415
Abhinivesh <abhisa8888@gmail.com>
2025-02-24 17:24:32 +0530

    landingpage

---
+++ client/client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPg from "../src/components/landingpage";
+// import LandingPg from "./components/landingpage";
+
+// import About from "./components/About";
+// import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div>
+      <LandingPg></LandingPg>
+    </div>
+  );
+}
+
+export default App;
