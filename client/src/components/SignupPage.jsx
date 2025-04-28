dabce9fb2f27ca36d86024c4fddfbdc07e3757e5
Abhinivesh <abhisa8888@gmail.com>
2025-04-28 11:37:39 +0530

    linked token

--- client/src/components/SignupPage.jsx
+++ client/src/components/SignupPage.jsx
@@ -32,7 +32,7 @@ function SignupPage() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("http://localhost:3000/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
